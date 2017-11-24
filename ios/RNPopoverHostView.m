15a623c467a8d599a42f0552e313942112f3f155
Bell <bell@greedlab.com>
2017-11-24 16:45:49 +0800

    fix: auto import RCTUIManagerUtils.h

--- ios/RNPopoverHostView.m
+++ ios/RNPopoverHostView.m
@@ -11,11 +11,14 @@
 #import <React/RCTAssert.h>
 #import <React/RCTBridge.h>
 #import <React/RCTUIManager.h>
-#import <React/RCTUIManagerUtils.h>
 #import <React/RCTUtils.h>
 #import <React/UIView+React.h>
 #import <React/RCTShadowView.h>
 #import <React/RCTTouchHandler.h>
+
+#if __has_include(<React/RCTUIManagerUtils.h>)
+#import <React/RCTUIManagerUtils.h>
+#endif
 
 #import "RNPopoverHostViewController.h"
 #import "RNPopoverTargetManager.h"
